6b890bc5739dead63310741504da8c6ed9a0fddd
YihengXiao <31838426+YihengXiao@users.noreply.github.com>
2018-08-22 23:39:52 -0400

    Add files via upload

---
+++ homework2/tests/test_tree.py
@@ -0,0 +1,35 @@
+import unittest
+from tree.print_tree import Node,Tree
+
+
+
+class TestPrintTree(unittest.TestCase):
+    def test1(self):
+        # single node as tree
+        case1=Node(1,None,None)
+        self.answer = "1\n"
+        self.input = Tree.PrintTree(case1)
+        assert self.input == self.answer
+
+    def test2(self):
+        # full balanced tree
+        case2=Node(1, Node(2,Node(4,None,None),Node(5,None,None)), Node(3,Node(6,None,None),Node(7,None,None)))
+
+
+        self.input = Tree.PrintTree(case2)
+        self.answer ="|||1|||\n|2|||3|\n4|5|6|7\n"
+        assert self.input == self.answer
+
+    def test3(self):
+        # all branches on the left
+        case3=Node(1, Node(2,Node(None,None,None),Node(5,None,None)), Node(3,Node(4,None,None),Node(None,None,None)))
+        self.input = Tree.PrintTree(case3)
+        self.answer ="|||1|||\n|2|||3|\n||5|4||\n"
+        assert self.input == self.answer
+
+    def test4(self):
+        # irregular tree
+        case4=Node(1, Node(2,Node(3,None,None),None),Node(None,Node(None,None,None),Node(None,None,None)))
+        self.input = Tree.PrintTree(case4)
+        self.answer ="|||1|||\n|2|||||\n3||||||\n"
+        assert self.input == self.answer
---
+++ homework2/tree/print_tree.py
@@ -0,0 +1,74 @@
+
+class Node(object):
+    def __init__(self, value,left,right):
+        """
+        Initialize Node
+        :param value: value of node
+        :attrib left: left node
+        :attrib right: right node
+        """
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class Tree(Node):
+    def __init__(self, root):
+        self.root = root
+
+    @classmethod
+    def PrintTree(self,root):
+        if not root:
+            return ""
+        q = [root]
+        res=[[root]]
+        while q:
+            tmp = [] # record current level,remove none
+            x = [] #record next level (children) = q
+            for item in q:
+                if item.left!=None and item.left.value!="*":
+                    x.append(item.left)
+                else:
+                    x.append(Node('*',None,None))
+                if item.right!=None and item.right.value!="*":
+                    x.append(item.right)
+                else:
+                    x.append(Node('*',None,None))
+            q = x
+            if len(q)>0 and len(list(filter(lambda x: x.value !='*', q)))>0 :
+                res.append(q)
+            else:
+                break
+        for i in range(len(res)):
+            for j in range(len(res[i])):
+                if res[i][j].value in [None,"*"]:
+                    res[i][j]="*"
+                else:
+                    res[i][j]=res[i][j].value
+
+        n=len(res)
+        out=""
+        for i in range(1,len(res)+1):
+            side="|"*(2**(n-i)-1)
+            if 2**(i-1)-1>0:
+                sep=int(((2**n-1)-(2**(n-i)-1)*2-2**(i-1))/(2**(i-1)-1))
+            else:
+                sep=0
+            sep="|"*sep
+            out+=side
+            for j in range(len(res[i-1])):
+                if j!=len(res[i-1])-1:
+                    if res[i-1][j] != "*":
+                        out+=str(res[i-1][j])
+                        out+=sep
+                    else:
+                        out+="|"
+                        out+=sep
+                else:
+                    if res[i-1][j]!="*":
+                        out+=str(res[i-1][j])
+                    else:
+                        out+="|"
+            out+=side
+            out+="\n"
+        return out
