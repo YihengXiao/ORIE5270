957959a3510e92e30263a28b100c8bf52808bdab
YihengXiao <31838426+YihengXiao@users.noreply.github.com>
2018-08-27 02:33:01 -0400

    Add files via upload

--- homework3/tests/test_find_path.py
+++ homework3/tests/test_find_path.py
@@ -8,7 +8,7 @@ class TestTree(unittest.TestCase):
     def test1_1(self):
         # Dijkstra Algo: search for shortest path
         # test case1_1: return shortest path
-        case1_1 = "./case1.txt"
+        case1_1 = "case1.txt"
         self.answer = (11, ['1', '2', '4', '7'])
         (self.input1, self.input2) = find_shortest_path(case1_1, source='1', destination='7')
         assert (self.input1, self.input2) == self.answer
@@ -16,7 +16,7 @@ class TestTree(unittest.TestCase):
     def test1_2(self):
         # Dijkstra Algo: search for shortest path
         # test case1_2: no feasible path
-        case1_2 = "./case1.txt"
+        case1_2 = "case1.txt"
         self.answer = (None, [])
         (self.input1, self.input2) = find_shortest_path(case1_2, source='6', destination='1')
         assert (self.input1, self.input2) == self.answer
@@ -24,7 +24,7 @@ class TestTree(unittest.TestCase):
     def test2_1(self):
         # Bellman-Ford Algo: search for shortest path
         # test case2_1: return shortest path
-        case2_1 = "./case1.txt"
+        case2_1 = "case1.txt"
         self.answer = (11, ['1', '2', '4', '7'])
         (self.input1, self.input2) = \
             find_shortest_path_Bellman(case2_1, source='1', destination='7')
@@ -33,7 +33,7 @@ class TestTree(unittest.TestCase):
     def test2_2(self):
         # Bellman-Ford Algo: search for shortest path
         # test case2_2: no feasible path
-        case2_2 = "./case1.txt"
+        case2_2 = "case1.txt"
         self.answer = (None, [])
         (self.input1, self.input2) = \
             find_shortest_path_Bellman(case2_2, source='6', destination='1')
@@ -41,7 +41,7 @@ class TestTree(unittest.TestCase):
 
     def test3(self):
         # Bellman-Ford Algo: detect negative cycle
-        case3 = "./case2.txt"
+        case3 = "case2.txt"
         self.answer = (-2, ['5', '7', '8', '5'])
         (self.input1, self.input2) = find_negative_cicles(case3)
         assert (self.input1, self.input2) == self.answer
