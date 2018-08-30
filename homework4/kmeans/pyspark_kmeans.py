bead20a1d70f703fbdefba03c3421f5a42ebe8fd
YihengXiao <31838426+YihengXiao@users.noreply.github.com>
2018-08-30 05:00:52 -0400

    Add files via upload

---
+++ homework4/kmeans/pyspark_kmeans.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+spark_home = os.environ.get('SPARK_HOME', None)
+sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.10.7-src.zip'))
+sys.path.insert(0, os.path.join(spark_home, 'python'))
+exec(open(os.path.join(spark_home, 'python/pyspark/shell.py')).read())
+
+import numpy as np
+import pyspark as ps
+import math
+import glob
+import re
+import time
+
+
+def save_results(centroids):
+    file = open("final_centroids.txt", "w")
+    for i in centroids:
+        pt = ""
+        for val in i:
+            pt += str(val) + " "
+        file.write(pt + "\n")
+    file.close()
+    return 1
+
+
+def k_means(data_file, centroids_file, MAX_ITER = 100, tol = 0.001):
+    """
+    :param data_file: path of raw data
+    :param centroids_file: path of initial guess of centroids
+    :param MAX_ITER: maximum iterations
+    :param tol: tolerance of convergence
+    :return:
+    """
+
+    # Load the data
+    data = sc.textFile(data_file).map(lambda line: np.array([float(x) for x in line.split(' ')])).cache()
+    ## load intial centriods and save it as a list
+    centroids_0 = sc.textFile(centroids_file).map(lambda line: np.array([float(x) for x in line.split(' ')])).collect()
+
+    for i in range(MAX_ITER):
+        pt_in_group = data.map(lambda m: (np.argmin([np.linalg.norm(m - n) for n in centroids_0]), (m, 1)))
+        centroids_1 = pt_in_group.reduceByKey(lambda x, y: (x[0]+y[0], x[1]+y[1])).sortByKey()
+        centroids_1 = centroids_1.map(lambda x: x[1][0]/x[1][1]).collect()
+        err = np.linalg.norm(np.array(centroids_1) - np.array(centroids_0))
+        print(str(i)+","+str(err))
+        if err <= tol:
+            print(str(i+1)+" iterations.\n"+"Error: "+str(err)+"Centroids:\n")
+            save_results(centroids_1)
+            break
+        centroids_0 = centroids_1[:]
+        save_results(centroids_1)
+    return 1
+
+if __name__ == '__main__':
+
+
+    data_file = 'data.txt'
+    centroids_file = 'c1.txt'
+    k_means(data_file, centroids_file, MAX_ITER= 100,tol=0.001)
+    sc.stop()
---
+++ homework4/matrix_multiplication/matrix_multiplication.py
@@ -0,0 +1,60 @@
+import os
+import sys
+spark_home = os.environ.get('SPARK_HOME', None)
+sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.10.7-src.zip'))
+sys.path.insert(0, os.path.join(spark_home, 'python'))
+exec(open(os.path.join(spark_home, 'python/pyspark/shell.py')).read())
+import pandas as pd
+import numpy as np
+import pyspark as ps
+import time
+
+
+def multiply(A, v):
+    matrices = sc.textFile(A)
+    vectors = sc.textFile(v)
+    """
+    input 1: matrix.txt
+    row idx, entry 0, entry 1, ..., entry n
+    0, a00, a00, ..., a0n
+    ...
+    input 2:vector.txt
+    entry 0
+    entry 1
+    ...
+    """
+    matrix = matrices.map(lambda l: [float(x) for x in l.split(",")]).cache()  # return a RDD
+    vector = vectors.map(lambda l: [float(x) for x in l.split(",")]).cache()
+
+    matrix = matrix.map(lambda l: (l[0], [(l[i], i-1) for i in range(1, len(l))]))
+    matrix = matrix.flatMap(lambda l: ((l[1][i][1], (l[0], l[1][i][0])) for i in range(len(l[1]))))
+    vector = vector.flatMap(lambda l: [(i, l[i]) for i in range(len(l))])
+
+    res = matrix.join(vector).map(lambda l: (l[1][0][0], l[1][0][1]*l[1][1]))
+    res = res.reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0]).map(lambda x: x[1]).collect()
+
+    return res
+
+if __name__ == '__main__':
+
+    n1=1000
+    n2=1000
+    a = pd.DataFrame(data = np.array(range(n1*n2)).reshape((n1,n2)),index=range(n1))
+    b = pd.DataFrame(data = np.array(range(1,n1+1)).reshape((1,n1)))
+    a.to_csv("matrix.txt",sep=',',header=False)
+    b.to_csv("vector.txt",index=None,sep=',',header=False)
+
+    sc = ps.SparkContext.getOrCreate()
+
+    start = time.clock()
+    A = "./matrix.txt"
+    v = "./vector.txt"
+    print(multiply(A,v))
+    elapsed = (time.clock() - start)
+    print(elapsed)
+    sc.stop()
+    ## Compare with np.dot()
+    start = time.clock()
+    print(np.dot(np.array(range(n1*n2)).reshape((n1,n2)),np.array(range(1,n1+1)).reshape((n1,1))))
+    elapsed = (time.clock() - start)
+    print(elapsed)
