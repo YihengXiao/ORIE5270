962fe248493da02c77601a84d4cd2cd46f37b644
YihengXiao <31838426+YihengXiao@users.noreply.github.com>
2018-08-31 18:06:45 -0400

    Update matrix_multiplication.py

--- homework4/matrix_multiplication/matrix_multiplication.py
+++ homework4/matrix_multiplication/matrix_multiplication.py
@@ -11,6 +11,12 @@ import time
 
 
 def multiply(A, v):
+    """
+    param: A, path of textfile of matrix A
+    param: v, path of textfile of vector v
+    return: res, a vector = Av
+    """
+    
     matrices = sc.textFile(A)
     vectors = sc.textFile(v)
     """
@@ -23,22 +29,22 @@ def multiply(A, v):
     entry 1
     ...
     """
-    matrix = matrices.map(lambda l: [float(x) for x in l.split(",")]).cache()  # return a RDD
-    vector = vectors.map(lambda l: [float(x) for x in l.split(",")]).cache()
+    matrix = matrices.map(lambda l: [float(x) for x in l.split(",")]).cache()  # read matrix A, split each entry by ','
+    vector = vectors.map(lambda l: [float(x) for x in l.split(",")]).cache()   # read vector v, split each entry by ','
 
-    matrix = matrix.map(lambda l: (l[0], [(l[i], i-1) for i in range(1, len(l))]))
-    matrix = matrix.flatMap(lambda l: ((l[1][i][1], (l[0], l[1][i][0])) for i in range(len(l[1]))))
+    matrix = matrix.map(lambda l: (l[0], [(l[i], i-1) for i in range(1, len(l))]))  # add index to each entry of A
+    matrix = matrix.flatMap(lambda l: ((l[1][i][1], (l[0], l[1][i][0])) for i in range(len(l[1])))) 
     vector = vector.flatMap(lambda l: [(i, l[i]) for i in range(len(l))])
 
-    res = matrix.join(vector).map(lambda l: (l[1][0][0], l[1][0][1]*l[1][1]))
-    res = res.reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0]).map(lambda x: x[1]).collect()
+    res = matrix.join(vector).map(lambda l: (l[1][0][0], l[1][0][1]*l[1][1]))  # multiply entry from matrix and vector
+    res = res.reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[0]).map(lambda x: x[1]).collect()  # compute Av
 
     return res
 
 if __name__ == '__main__':
 
-    n1=1000
-    n2=1000
+    n1=10
+    n2=10
     a = pd.DataFrame(data = np.array(range(n1*n2)).reshape((n1,n2)),index=range(n1))
     b = pd.DataFrame(data = np.array(range(1,n1+1)).reshape((1,n1)))
     a.to_csv("matrix.txt",sep=',',header=False)
